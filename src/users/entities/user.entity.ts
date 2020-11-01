b4c445fc7b6c78b2ecc2a5bf5d28b26e1b97dc83
anpigon <markan82@gmail.com>
2020-11-01 21:26:29 +0900

    #4.4 Create Account Mutation part Two

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,19 @@
-import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import {
+  Field,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
 import { CoreEntity } from 'src/common/entities/core.entities';
 import { Column, Entity } from 'typeorm';
 
-type UserRole = 'client' | 'owner' | 'delivery';
+enum UserRole {
+  Owner,
+  Client,
+  Delivery,
+}
+
+registerEnumType(UserRole, { name: 'UserRole' });
 
 @InputType({ isAbstract: true })
 @ObjectType()
@@ -16,7 +27,10 @@ export class User extends CoreEntity {
   @Field()
   password: string;
 
-  @Column()
-  @Field()
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+  })
+  @Field(() => UserRole)
   role: UserRole;
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { of } from 'rxjs';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dtos/create-account.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -8,4 +10,26 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
+
+  async createAccount(createAccountInput: CreateAccountInput) {
+    const { email, password, role } = createAccountInput;
+    try {
+      const exists = await this.users.findOne({ email });
+      if (exists) {
+        // make error
+        return;
+      }
+      await this.users.save(
+        this.users.create({
+          email,
+          password,
+          role,
+        }),
+      );
+      return true;
+    } catch (e) {
+      // make error
+      return;
+    }
+  }
 }
