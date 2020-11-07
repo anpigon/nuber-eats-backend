9ef5b2c85801f03d076a96f8e34f87fb248d1b53
anpigon <markan82@gmail.com>
2020-11-07 19:25:34 +0900

    #6.8 Beautiful Emails

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,8 +1,7 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { CONFIG_OPTIONS } from 'src/common/common.contants';
 import { MailModuleInterface } from './mail.interfaces';
-import got from 'got';
-import * as FormData from 'form-data';
+import * as mailgun from 'mailgun-js';
 
 @Injectable()
 export class MailService {
@@ -10,28 +9,20 @@ export class MailService {
     @Inject(CONFIG_OPTIONS) private readonly options: MailModuleInterface,
   ) {}
 
-  private async sendMail(subject: string, content: string, to: string) {
-    try {
-      const form = new FormData();
-      form.append('from', `Excited User <${this.options.domain}>`);
-      form.append('to', to);
-      form.append('subject', subject);
-      form.append('text', content);
-      const response = await got(
-        `https://api.mailgun.net/v3/${this.options.domain}/messages`,
-        {
-          method: 'POST',
-          headers: {
-            Authorization: `Basic ${Buffer.from(
-              `api:${this.options.apiKey}`,
-            ).toString('base64')}`,
-          },
-          body: form,
-        },
-      );
-      console.log(response.body);
-    } catch (error) {
-      console.error(error);
-    }
+  private async sendMail(subject: string, template: string, to: string) {
+    const mg = mailgun({
+      apiKey: this.options.apiKey,
+      domain: this.options.domain,
+    });
+    const data = {
+      from: `Excited User <mailgun@${this.options.domain}>`,
+      to,
+      subject,
+      template,
+      'v:code': 'code',
+      'v:username': 'username',
+    };
+    const response = await mg.messages().send(data);
+    console.log(response);
   }
 }
