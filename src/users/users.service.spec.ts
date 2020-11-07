3682e227098de17d2386a407e869938ed300d3b1
anpigon <markan82@gmail.com>
2020-11-07 20:36:27 +0900

    #7.2 Mocking part Two

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,6 +2,7 @@ import { Test } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { JwtService } from 'src/jwt/jwt.service';
 import { MailService } from 'src/mail/mail.service';
+import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { Verification } from './entities/verification.entity';
 import { UsersService } from './users.service';
@@ -18,11 +19,14 @@ const mokeJwtService = {
 };
 
 const mokeMailService = {
-  sendVerificationEmail: jest.fn()
-}
+  sendVerificationEmail: jest.fn(),
+};
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
 
 describe('UserService', () => {
   let service: UsersService;
+  let usersRepository: MockRepository<User>;
 
   beforeAll(async () => {
     const modules = await Test.createTestingModule({
@@ -47,13 +51,19 @@ describe('UserService', () => {
       ],
     }).compile();
     service = modules.get<UsersService>(UsersService);
+    usersRepository = modules.get<typeof usersRepository>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('createAccount');
+  describe('createAccount', () => {
+    it('should fail if user exists', () => {
+      // service.createAccount(...)
+    });
+  });
+
   it.todo('login');
   it.todo('findById');
   it.todo('editProfile');
