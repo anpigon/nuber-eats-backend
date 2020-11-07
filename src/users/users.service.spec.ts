06455900ca684bbb68b1e475ac1946d38959cad4
anpigon <markan82@gmail.com>
2020-11-07 20:27:36 +0900

    #7.1 Mocking

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,50 @@
 import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { JwtService } from 'src/jwt/jwt.service';
+import { MailService } from 'src/mail/mail.service';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
 import { UsersService } from './users.service';
+
+const mockUserRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mokeJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mokeMailService = {
+  sendVerificationEmail: jest.fn()
+}
 
 describe('UserService', () => {
   let service: UsersService;
 
   beforeAll(async () => {
     const modules = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockUserRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mokeJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mokeMailService,
+        },
+      ],
     }).compile();
     service = modules.get<UsersService>(UsersService);
   });
