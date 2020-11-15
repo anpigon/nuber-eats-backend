2ea2c0072a71d26a5f3aaff100cd7da1a21a2157
anpigon <markan82@gmail.com>
2020-11-15 16:45:38 +0900

    #9.0 Setup part One

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,7 +24,7 @@ import { MailModule } from './mail/mail.module';
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
         NODE_ENV: Joi.string()
-          .valid('dev', 'prod')
+          .valid('dev', 'prod', 'test')
           .default('dev'),
         PORT: Joi.number().default(3000),
         DB_HOST: Joi.string().required(),
