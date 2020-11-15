45495ffd8237f567248f0d47e4bacec035ad9d38
anpigon <markan82@gmail.com>
2020-11-15 14:40:03 +0900

    #8.1 JWT Sign Test

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,6 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as jwt from 'jsonwebtoken';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
+
+jest.mock('jsonwebtoken', () => {
+  return {
+    sign: jest.fn(() => 'TOKEN'),
+  };
+});
 
 const TEST_KEY = 'testKey';
 const USER_ID = 1;
@@ -26,6 +33,22 @@ describe('JwtService', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('sign')
-  it.todo('verify')
+  describe('sign', () => {
+    it('should return a signed token', async () => {
+      const token = service.sign(USER_ID);
+
+      expect(typeof token).toBe('string');
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: USER_ID }, TEST_KEY);
+
+      expect(token).toEqual('TOKEN');
+    });
+  });
+
+  describe('verify', () => {
+    it('should return a decoded token', async () => {
+      // next video
+    });
+  });
+
 });
