6225791d22eeda097805f2c45efd28d9639f2b33
anpigon <markan82@gmail.com>
2020-11-25 22:05:40 +0900

    #10.15 Pagination

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -127,11 +127,14 @@ export class RestaurantService {
     return this.restaurants.count({ category });
   }
 
-  async findCategoryBySlug({ slug }: CategoryInput): Promise<CategoryOutput> {
+  async findCategoryBySlug({
+    slug,
+    page,
+  }: CategoryInput): Promise<CategoryOutput> {
     try {
       const category = await this.categories.findOne(
         { slug },
-        { relations: ['restaurants'] },
+        // { relations: ['restaurants'] },
       );
       if (!category) {
         return {
@@ -139,9 +142,19 @@ export class RestaurantService {
           error: 'Category not found',
         };
       }
+      const restaurants = await this.restaurants.find({
+        where: {
+          category,
+        },
+        take: 25,
+        skip: (page - 1) * 25,
+      });
+      category.restaurants = restaurants;
+      const totalResults = await this.countRestaurants(category);
       return {
         ok: true,
         category,
+        totalPages: Math.ceil(totalResults / 25),
       };
     } catch (error) {
       return {
