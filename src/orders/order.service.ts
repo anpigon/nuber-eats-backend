593d26f158f69013f160bcc0a7e86abd1be8bab8
anpigon <markan82@gmail.com>
2020-12-27 12:10:36 +0900

    22.6 Making Order part Six

--- src/orders/order.service.ts
+++ src/orders/order.service.ts
@@ -95,7 +95,7 @@ export class OrderService {
       await this.pubSub.publish(NEW_PENDING_ORDER, {
         pendingOrders: { order, ownerId: restaurant.ownerId },
       });
-      return { ok: true };
+      return { ok: true, orderId: order.id  };
     } catch (error) {
       return {
         ok: false,
